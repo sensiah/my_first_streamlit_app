5dfc15e140f4a56476a64e0912af313dcf43449d
sensiah <142753134+sensiah@users.noreply.github.com>
2023-11-02 00:26:56 +0100

    Add files via upload

---
+++ Play_with_Data.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.title("ITSFA Project")
+st.markdown('''
+            <div style="text-align: center;">هذا المشروع قيد التطوير، وهو حاليا مجرد تطبيق لما تعلمته في دورة علم البيانات</div>
+            <div style="text-align: center;">Thank you Dr Soufiane Ouari</div>
+            <div style="text-align: center;">Thank you Limitless</div>
+            ''', unsafe_allow_html=True)
