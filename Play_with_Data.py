956f0b3c1e14f03bd78c873a3f8a0fd70782515b
sensiah <142753134+sensiah@users.noreply.github.com>
2023-11-02 18:34:52 +0100

    Update Play_with_Data.py

--- Play_with_Data.py
+++ Play_with_Data.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 
-st.title("ITSFA Project")
+
+st.markdown("<h1 style='text-align: center; '>Just A Project</h1>", unsafe_allow_html=True)
 st.markdown('''
             <div style="text-align: center;">هذا المشروع قيد التطوير، وهو حاليا مجرد تطبيق لما تعلمته في دورة علم البيانات</div>
             <div style="text-align: center;">Thank you Dr Soufiane Ouari</div>
