086a2f360d421bff882574bb2db4ef19c4e465d1
sensiah <142753134+sensiah@users.noreply.github.com>
2023-11-02 18:51:49 +0100

    Update 4_🌴_Palm_Date_identifier.py

--- pages/4_🌴_Palm_Date_identifier.py
+++ pages/4_🌴_Palm_Date_identifier.py
@@ -2,15 +2,6 @@ import streamlit as st
 import time
 
 
-#Title and Overview
-st.markdown("<h1 style='text-align: center; '>Varieties of Dates</h1>", unsafe_allow_html=True)
-st.write("The presentation below is an introduction the varieties of dates in Algeria and their distribution according to regions of production, in addition to information about the amount of production and export...etc.")
-
-#Add the presentation from google Docs
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQx4f3CC651oqTXrYihiYWno4LxbkLmHHVJ01QAcbwMXThJyNvcWEAzvimNSo3_Ow/embed?start=false&loop=false&delayms=3000", height=565)
-
-#Upload A date variety 
-st.title("Check your date varity by uploding an image")
 file = st.file_uploader("Please Enter your palm date", type=["jpg"])
 
 if file is not None:
