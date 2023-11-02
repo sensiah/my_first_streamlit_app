68b67fdd23479439bbd4c9b8079dd8afbd23e562
sensiah <142753134+sensiah@users.noreply.github.com>
2023-11-02 18:47:05 +0100

    Update 1_📊_Data_analyzer.py

--- pages/1_📊_Data_analyzer.py
+++ pages/1_📊_Data_analyzer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 
+#Upload file
 file = st.file_uploader("Upload your file", type=["csv"])
 
 if file is not None:
@@ -11,25 +12,27 @@ if file is not None:
     # Add a slider to control number of rows
     n_rows = st.slider("Choose number of rows", min_value=3, max_value=len(df), step=1)
 
-    # Choose specific columns --> df.columns to show all columns
+    # Choose specific columns --> df.columns to show all columns and transfert it to list
     n_columns = st.multiselect("Select columns", df.columns.to_list(), default=df.columns.to_list())
 
     #Print the table
     st.write(df[:n_rows][n_columns])
 
     # Show Tabs
-    tab1, tab2 = st.tabs(["Scatter", "Histogram"])
+    tab1, tab2, tab3 = st.tabs(["Scatter", "Histogram", "PieChart"])
     numerical_columns = df.select_dtypes(include=np.number).columns.to_list()
 
     #Scatter
     with tab1:
-        col1, col2 = st.columns(2)
+        col1, col2, col3 = st.columns(3)
         with col1:
-            x_column = st.selectbox("Select feature on X axis", numerical_columns)
+            x_column = st.selectbox("Select feature on X axis", list(df.columns))
         with col2:
-            y_column = st.selectbox("Select features on Y axis", numerical_columns)
+            y_column = st.selectbox("Select features on Y axis", list(df.columns))
+        with col3:
+            colors = st.selectbox("Select column as color", list(df.columns))
 
-        fig_scatter = px.scatter(df, x=x_column, y=y_column)
+        fig_scatter = px.scatter(df, x=x_column, y=y_column, color=colors)
         st.plotly_chart(fig_scatter)
 
     #Histogram
@@ -37,3 +40,6 @@ if file is not None:
         histogram_feature = st.selectbox("Select feature", numerical_columns)
         fig_histogram = px.histogram(df, x=histogram_feature)
         st.plotly_chart(fig_histogram)
+    
+    with tab3:
+        pass
